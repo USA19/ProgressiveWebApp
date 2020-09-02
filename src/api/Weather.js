2a3cd41cbcc4a1abb3236b711ab858c6934ecb13
Usama Sarfraz <muhamedusama648@gmail.com>
2020-09-02 20:39:41 +0500

    Update Weather.js

--- src/api/Weather.js
+++ src/api/Weather.js
@@ -1,8 +1,7 @@
 import axios from 'axios';
 
 const URL = 'https://api.openweathermap.org/data/2.5/weather';
-const API_KEY='76534a0c8b1a75ea2d2285ded558f98b';
-// const API_KEY = 'f33a484cf794d08d0148764789aaba32';
+const API_KEY='Your API key here'
 
 export const Weather = async (query) => {
     const { data } = await axios.get(URL, {
